2c402114e0e38dcc90d5c03f491cc1d041c98cf8
langsmith <langstonlmcs@gmail.com>
2019-09-19 16:12:53 -0700

    [android] adding support for adjusting focus in MapView

--- platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/MapView.java
+++ platform/android/MapboxGLAndroidSDK/src/main/java/com/mapbox/mapboxsdk/maps/MapView.java
@@ -131,9 +131,20 @@ public class MapView extends FrameLayout implements NativeMapView.ViewCallback {
 
     // inflate view
     View view = LayoutInflater.from(context).inflate(R.layout.mapbox_mapview_internal, this);
+    view.setNextFocusForwardId(R.id.compassView);
+    view.setNextFocusRightId(R.id.compassView);
+    view.setNextFocusLeftId(R.id.attributionView);
     compassView = view.findViewById(R.id.compassView);
+    compassView.setNextFocusForwardId(R.id.attributionView);
+    compassView.setNextFocusRightId(R.id.attributionView);
+    compassView.setNextFocusLeftId(view.getId());
     attrView = view.findViewById(R.id.attributionView);
     attrView.setImageDrawable(BitmapUtils.getDrawableFromRes(getContext(), R.drawable.mapbox_info_bg_selector));
+    attrView = view.findViewById(R.id.attributionView);
+    attrView.setImageDrawable(BitmapUtils.getDrawableFromRes(getContext(), R.drawable.mapbox_info_bg_selector));
+    attrView.setNextFocusForwardId(view.getId());
+    attrView.setNextFocusRightId(R.id.compassView);
+    attrView.setNextFocusLeftId(view.getId());
     logoView = view.findViewById(R.id.logoView);
     logoView.setImageDrawable(BitmapUtils.getDrawableFromRes(getContext(), R.drawable.mapbox_logo_icon));
 
